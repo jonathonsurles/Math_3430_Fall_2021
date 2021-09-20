d6b48d24afb6227657dc16ae50021846eb38b491
Jonathon <jonathonsurles@gmail.com>
2021-09-20 14:05:58 -0500

    Inserted working versions of code, to reformat later

--- HW02.py
+++ HW02.py
@@ -66,22 +66,306 @@ def add_vectors(vector_a,vector_b):
 
 
 # Problem 01
+'''
+Write an algorithm to implement scalar-vector multiplication.
 
+Q1: What do we have?
+A1: A vector "vector" stored as a list and a scalar "scalar" stored as an integer
+
+Q2: What do we want?
+A2: The product of vector and scalar stored as a list
+
+Q3: How will we get there?
+A3: Create an empty list and then store vector times scalar by appending, in
+    order, the scalar multiplied by each element of vector
+
+def vector_scalar_multiply(vector, scalar):
+
+  # Create a new list to server as our resultant vector
+  product_vector = []
+
+  # Compute the product by calculating each element then append it to result
+  for element in vector:
+    append (element * scalar) to product_vector
+
+  # Return our product vector
+  return product_vector
+'''
+def vector_scalar_multiply(vector, scalar):
+    '''
+    Input:  vector: A vector stored as a list 
+            scalar: A scalar stored as a number
+    
+    Output: The product of vector and scalar stored as a list
+    
+    Method: Create an empty list and then store vector times scalar by
+            appending, in order, the scalar multiplied by each element of
+            vector
+    '''
+
+    # Create a new list to server as our resultant vector
+    product_vector = []
+    
+    # Compute the product by calculating each element then append it to result
+    for element in vector:
+        product_vector.append(element * scalar)
+
+    # Return our product vector
+    return product_vector
+ 
 
 # Problem 02
+'''
+Write an algorithm to implement scalar-matrix multiplication.
+
+Q1: What do we have?
+A1: A matrix "matrix" stored as a 2D list where each component list represents
+    a column vector, and a scalar "scalar" stored as an integer
+
+Q2: What do we want?
+A2: The product of matrix and scalar stored as a 2D list where each component
+    lists represents a column vector
+
+Q3: How will we get there?
+A3: Create an empty list then append in order each component vector of matrix 
+    multiplied by the scalar
+
+def matrix_scalar_multiply(matrix, scalar):
+  
+  # Create a new list to serve as our resultant matrix
+  product_matrix = []
+
+  # Compute the product by calculating the product of each vector times the
+  # scalar, and appending it to our result
+  for vector in matrix:
+    append vector_scalar_multiply(vector, scalar) to product_matrix
+
+  # Return our product matrix
+  return product_matrix
+'''
+def matrix_scalar_multiply(matrix, scalar):
+    '''
+    Input:  matrix: A matrix stored as a 2D list where each component list
+                represents a vector
+            scalar: A scalar stored as a number
+    
+    Output: The product of matrix and scalar, stored as a 2D list, where each
+            component list represents a vector, similar to the input matrix
+    
+    Method: Create an empty list then append, in order, each component vector
+            of matrix multiplied by the scalar using vector_scalar_multiply()
+    '''
+
+    # Create a new list to serve as our resultant matrix
+    product_matrix = []
+    
+    # Compute the product by calculating the product of each vector times the
+    # scalar, and appending it to our result
+    for vector in matrix:
+        product_matrix.append(vector_scalar_multiply(vector, scalar))
+    
+    # Return our product matrix
+    return product_matrix
+
 
 
 # Problem 03
+'''
+Write an algorithm to implement matrix addition.
+
+Q1: What do we have?
+A1: Two matrices "matrix_a" and "matrix_b", stored as a 2D list, where each
+    component list represents a column vector. Assumed to be the same size.
+
+Q2: What do we want?
+A2: The sum of matrix_a and matrix_b, stored as a 2D list, where each
+    component list represents a column vector.
+
+Q3: How will we get there?
+A3: Create an empty list then append in order the sum of each corresponding
+    component vector of matrix_a and matrix_b
+
+def matrix_add(matrix_a, matrix_b):
+
+  # Create a new list to server as our resultant matrix
+  matrix_sum = []
+
+  # Compute the sum by calculating the sum of each pair of corresponding
+  # vectors in matrix_a and matrix_b
+  for index in range(length(matrix_a)):
+    append add_vectors(matrix_a[index], matrix_b[index]) to matrix_sum
+
+  # Return our sum matrix
+  return matrix_sum
+'''
+def matrix_add(matrix_a, matrix_b):
+    '''
+    Input:  matrix_a, matrix_b: Two matrices stored as 2D lists, where each
+                component list represents vectors of the same type as each
+                other (rows/rows, or columns/columns). Assumed to be the same
+                size.
+    
+    Output: The sum of matrix_a and matrix_b, stored as a 2D list, where each
+                component list represents a vector of the same type as the
+                input matrices (rows or columns).
+    
+    Method: Create an empty list then append, in order, the sum of each 
+            corresponding component vector of matrix_a and matrix_b using
+            vector_add()
+    '''
+    # Create a new list to server as our resultant matrix
+    matrix_sum = []
+    
+    # Compute the sum by calculating the sum of each pair of corresponding
+    # vectors in matrix_a and matrix_b
+    for index in range(len(matrix_a)):
+        matrix_sum.append(vector_add(matrix_a[index], matrix_b[index]))
+    
+    # Return our sum matrix
+    return matrix_sum
+
 
 
 # Problem 04
+'''
+Write an algorithm to implement matrix-vector multiplication using the linear
+combination of columns method. You must use the algorithms from Problem #0 and
+Problem #1.  
+
+Q1: What do we have?
+A1: A matrix "matrix" stored as a 2D list, where each component list
+    represents a column vector, and a column vector "vector", store as a list.
+    Assumed to be compatible to multiply.
+
+Q2: What do we want?
+A2: The matrix-vector product of matrix and vector stored as a 2D list, where
+    each component list represents a column vector.
+
+Q3: How will we get there?
+A3: Use the linear combination of columns method to multiply matrix and vector.
+    I.e. multiply each column of matrix by the corresponding element of vector,
+    then sum each resulting vector and return that final resultant vector.
+
+def matrix_vector_multiply(matrix, vector):
+  
+  # Initialize a resultant vector full of 0s
+  product_vector = []
+
+  for element in matrix[0]:
+    append 0 to product_vector
+
+  # Initialize a list to store our intermediate vectors
+  inter_vectors = []
+  
+  # Calculate the product of each column of matrix with the corresponding
+  # element of vector
+  for index in range(length(vector)):
+    append vector_scalar_multiply(matrix[index], vector[index]) to inter_vectors
+
+  # Calculate the final result by adding each intermediate vector
+  for inter_vector in inter_vectors:
+    product_vector = add_vectors(product_vector, inter_vector)
+
+  # Return our product vector
+  return product_vector
+'''
+def matrix_vector_multiply(matrix, vector):
+    '''
+    Input:  A matrix "matrix" stored as a 2D list, where each component list
+            represents a column vector, and a column vector "vector", stored
+            as a list. Assumed to be compatible to multiply.
+    
+    Output: The matrix-vector product of matrix and vector stored as a list,
+            where matrix is the left operand and vector the right.
+    
+    Method: Use the linear combination of columns method to multiply matrix and
+            vector. I.e. multiply each column of matrix by the corresponding 
+            element of vector, store those vectors, finally, sum each resulting
+            vector and return that final vector.
+    '''
+    # Initialize a resultant vector full of 0s
+    product_vector = []
+    
+    for element in matrix[0]:
+        product_vector.append(0)
+    
+    # Initialize a list to store our intermediate vectors
+    inter_vectors = []
+    
+    # Calculate the product of each column of matrix with the corresponding
+    # element of vector, and store it as an intermediate vector
+    for index in range(len(vector)):
+        inter_vectors.append(vector_scalar_multiply(matrix[index], vector[index]))
+    
+    # Calculate the final result by adding each intermediate vector
+    for inter_vector in inter_vectors:
+        product_vector = vector_add(product_vector, inter_vector)
+    
+    # Return our product vector
+    return product_vector
+
 
 
 # Problem 05
+'''
+Write an algorithm to implement matrix-matrix multiplication using your
+algorithm from Problem #4.  
+
+Q1: What do we have?
+A1: Two matrices, "left_matrix" and "right_matrix", each stored as a 2D list
+    where each component list represents a column vector. Assumed to be
+    compatible to multiply.
+
+Q2: What do we want?
+A2: A matrix that is the product of left_matrix and right_matrix, stored as a
+    2D list where each component list represents a column vector.
+
+Q3: How will we get there?
+A3: Create an empty list to represent the resultant matrix, then calculate each
+    column using a linear combination of columns and append to the resultant
+    matrix.
+
+def matrix_multiply(left_matrix, right_matrix):
+  
+  # Define our resultant matrix
+  matrix_product = []
+
+  # Calculate each column using linear combination of columns and append to
+  # the resultant matrix
+  for column in right_matrix:
+    append matrix_vector_multiply(left_matrix, column) to matrix_product
+
+  # Return our resultant matrix
+  return matrix_product
+'''
+def matrix_multiply(left_matrix, right_matrix):
+    '''
+    Input:  left_matrix, right_matrix: Two matrices, each stored as a 2D list
+            where each component list represents a column vector. Assumed to be
+            compatible to multiply.
+    
+    Output: A matrix that is the product of left_matrix and right_matrix,
+            stored as a 2D list where each component list represents a column
+            vector.
+    
+    Method: Create an empty list to represent the resultant matrix, then
+            calculate each column using matrix_vector_multiply(), appending to
+            the resultant matrix as they are calculated.
+    '''
+
+    # Define our resultant matrix
+    matrix_product = []
+    
+    # Calculate each column using linear combination of columns and append to
+    # the resultant matrix
+    for column in right_matrix:
+        matrix_product.append(matrix_vector_multiply(left_matrix, column))
+    
+    # Return our resultant matrix
+    return matrix_product
 
 
-
-# Test if file run directly
+# Test if file is run directly
 if __name__ == '__main__':
     
     # Variables for testing
