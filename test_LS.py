dd79545121652c30ac5b83e26fc82bcdc3ba4129
Jonathon <jonathonsurles@gmail.com>
2021-11-15 13:34:20 -0600

    Nonsquare no-solution matrix test

--- test_LS.py
+++ test_LS.py
@@ -9,6 +9,7 @@ def test_least_squares():
     """Tests LS.least_squares()"""
     assert LS.least_squares([[2, 0, 0], [4, 4, 0], [3, 1, 6]], [9, 5, 6]) \
             == [1, 1, 1]
+    assert LS.least_squares([[1, 1, 1], [0, 1, 2]], [6, 0, 0]) == [5, -3]
 
 
 if __name__ == '__main__':
