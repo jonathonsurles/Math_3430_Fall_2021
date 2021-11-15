c7da2983fbdc94c4cf168c659fe675eba07ae45e
Jonathon <jonathonsurles@gmail.com>
2021-11-15 14:26:42 -0600

    Made lint.py actually work

--- lint.py
+++ lint.py
@@ -6,15 +6,15 @@ from pylint.lint import Run
 
 def main():
     """Runs pylint over each file in this project"""
+    args = ['-j 0']
     files = ['LA.py',
              'test_LA.py',
              'QR.py',
              'test_QR.py',
              'LS.py',
              'test_LS.py',
-             'pylint.py']
-    for file_ in files:
-        Run([file_])
+             'lint.py']
+    Run(args + files)
 
 
 if __name__ == '__main__':
