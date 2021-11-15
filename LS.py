867b88a4205764f9d47c457cbe0a6fc2c9747f07
Jonathon <jonathonsurles@gmail.com>
2021-11-15 12:42:15 -0600

    Grand plan for HW8

--- LS.py
+++ LS.py
@@ -22,3 +22,10 @@ def least_squares(matrix: Matrix, vector: Vector) -> Vector:
     Returns:
         The vector x in an equation Ax = b
     """
+    '''Plan
+    Let Q, R such that QR = A and R is upper triangular and square
+    Calculate the vector c = Q*b, which is equal to Rx
+    Create an empty vector of the appropriate size to store x
+    Use back subsitution to find the elements of x, since R is triangular
+    Return R
+    '''
