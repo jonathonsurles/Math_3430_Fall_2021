681cf1d237bb7bc3846bb488fc2e7940dfc07696
Jonathon <jonathonsurles@gmail.com>
2021-11-15 12:49:36 -0600

    Single test for least squares

--- test_LS.py
+++ test_LS.py
@@ -1,1 +1,11 @@
 """Tests functions in LS.py."""
+
+
+import pytest
+import LS
+
+
+def test_least_squares():
+    """Tests LS.least_squares()"""
+    assert LS.least_squares([[2, 0, 0], [4, 4, 0], [3, 1, 6]], [9, 5, 6]) \
+            == [1, 1, 1]
