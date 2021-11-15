f60679e546ab26b23483850650648b84a9366b01
Jonathon <jonathonsurles@gmail.com>
2021-11-15 12:38:06 -0600

    Specifications for homework 8

--- LS.py
+++ LS.py
@@ -1,1 +1,24 @@
 """Contains functions to find the least squares solutions of equations."""
+
+
+import LA
+import QR
+
+
+# Type aliases
+Vector = list[complex]
+Matrix = list[list[complex]]
+
+
+def least_squares(matrix: Matrix, vector: Vector) -> Vector:
+    """Finds the least squares solution of a matrix-vector product, Ax = b
+
+    ...
+
+    Args:
+        matrix: The matrix A in an equation Ax = b
+        vector: The vector b in an equation Ax = b
+
+    Returns:
+        The vector x in an equation Ax = b
+    """
