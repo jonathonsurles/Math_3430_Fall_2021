d7ca079a341252893bcb282d65c58dc02052e0f1
Jonathon <jonathonsurles@gmail.com>
2021-11-22 12:45:38 -0600

    Submissions for homeworks 9 & 10

--- Demo.py
+++ Demo.py
@@ -9,39 +9,47 @@ import test_QR
 import test_LS
 
 
-Test = tuple[tuple, object]
+Case = tuple[tuple, object]
 
 
 def main():
     """Demos the functions in this package"""
     # Sometimes the desire to not type as much leads a programmer (me,
     # in this case) to learn a lot about the language they're using
-    code_mods: dict = {'LA':LA,
-                       'QR':QR,
-                       'LS':LS}
-    test_mods: dict = {'test_LA':test_LA,
-                       'test_QR':test_QR,
-                       'test_LS':test_LS}
+    # These dictionaries allow us to find modules based on their names
+    code_mods: dict[module] = {'LA':LA,
+                               'QR':QR,
+                               'LS':LS}
+    test_mods: dict[module] = {'test_LA':test_LA,
+                               'test_QR':test_QR,
+                               'test_LS':test_LS}
+    # m_name is a string, m_val is a module
     for m_name, m_val in code_mods.items():
-        # Module header
+        # Print module header
         print(f'Module {m_name}: {m_val.__doc__}')
-        # Build a dictionary of all functions in the current module
-        m_fun = {obj:getattr(m_val, obj)
-                for obj in dir(m_val) if obj[0] != '_' and str.islower(obj[0])}
         # The test module for the current code module
-        t_mod = test_mods['test_' + m_name]
+        t_mod: module = test_mods['test_' + m_name]
+        # Build a dictionary of all functions in the current module
+        m_fun: dict[function] = {obj:getattr(m_val, obj)
+                for obj in dir(m_val) if obj[0] != '_' and str.islower(obj[0])}
         # Iterate over each of these functions and print docstring and test
         for f_name, f_val in m_fun.items():
-            shortdoc = f_val.__doc__[:f_val.__doc__.index('\n')]
+            # Grab and print the one-line documentation for the function
+            shortdoc: string = f_val.__doc__[:f_val.__doc__.index('\n')]
             print(f'  Function {m_name}.{f_name}: {shortdoc}')
-            # The test function for the current code function
+            # Print an example of function usage
             try:
-                t_fun = getattr(t_mod, 'test_' + f_name)  # The test function
-                test = t_fun()  # The test function returns a test case, so use that
+                # Find the test function for the current code function
+                t_fun: function = getattr(t_mod, 'test_' + f_name)
+                # Tests have been written to return an example test case,
+                # so grab it and print it.
+                case: Case = t_fun()
+                args: str = ', '.join(map(str, case[0]))
+                rslt: str = str(case[1])
                 print('    Example use case:')
-                print(f'    {m_name}.{f_name}({", ".join(map(str, test[0]))}) = {test[1]}')
+                print(f'    {m_name}.{f_name}({args}) = {rslt}')
             except AssertionError:
-                print("    Actually that function is broken don't use it")
+                print("    ERROR: Failed test. Do not use.")
             except AttributeError:
                 continue
 
